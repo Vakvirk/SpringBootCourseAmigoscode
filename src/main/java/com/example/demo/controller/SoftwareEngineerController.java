bed1199eed972b158f659b60cedfac44d5c727a2
Bartosz Lewandowicz <bartosz.lewandowicz@gmail.com>
2025-07-03 11:36:00 +0200

    Dodano kontroler dla SoftwareEngineer

---
+++ src/main/java/com/example/demo/controller/SoftwareEngineerController.java
@@ -0,0 +1,29 @@
+
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.SoftwareEngineer;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "Bob",
+                        "MEAN"),
+                new SoftwareEngineer(
+                        2,
+                        "Adam",
+                        "LAMP"));
+    }
+
+}
