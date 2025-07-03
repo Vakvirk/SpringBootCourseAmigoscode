3b326b0fe408ec4df91b86747be1ec04c072fa95
Bartosz Lewandowicz <bartosz.lewandowicz@gmail.com>
2025-07-03 11:07:07 +0200

    Dodano model SoftwareEngineer

---
+++ src/main/java/com/example/demo/model/SoftwareEngineer.java
@@ -0,0 +1,88 @@
+package com.example.demo.model;
+
+public class SoftwareEngineer {
+
+    // pola
+
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    // konstruktory
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(Integer id, String name, String techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    // settery i gettery
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    // equals() i hashCode(), wykorzystywane do porównywania obiektów w celu
+    // poprawnego działania kolekcji (HashMap, HashSet)
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((techStack == null) ? 0 : techStack.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SoftwareEngineer other = (SoftwareEngineer) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (techStack == null) {
+            if (other.techStack != null)
+                return false;
+        } else if (!techStack.equals(other.techStack))
+            return false;
+        return true;
+    }
+
+}
